4a9a16d40c9914303dbd1d1b4ce7faad28ac2e6c
weriks <ti15@viacometa.com.br>
2022-06-02 12:49:03 -0400

    refactor: adicinar style

--- src/App.vue
+++ src/App.vue
@@ -58,3 +58,20 @@ export default {
   }),
 };
 </script>
+
+<style>
+#app {
+  background: linear-gradient(
+      to bottom right,
+      rgba(10, 10, 10, 1),
+      rgba(12, 39, 63, 1)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+}
+</style>
