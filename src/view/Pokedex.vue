2f66cb949016f1a59570ac3f81854a6075308b52
weriks <ti15@viacometa.com.br>
2022-07-14 12:16:57 -0400

    feat: inicia template da pokedex

---
+++ src/view/Pokedex.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container class="container">
+    <v-img
+      class="pokedex-image"
+      width="800" 
+      :src="require('@/assets/pokedex.png')"
+    >
+    </v-img>
+    <v-card class="pokemon-card"></v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .pokedex-image{
+    margin: 0 auto !important;
+    z-index: 2;
+  }
+  .pokemon-card{
+    background-color: #fff;
+    width: 255px;
+    height: 170px;
+    position: absolute;
+    top: 365px;
+    left: 635px;
+  }
+</style>
