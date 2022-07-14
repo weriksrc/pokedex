4f76078f3fc4c60102689ecd862ed55698c577a7
weriks <ti15@viacometa.com.br>
2022-07-14 13:37:21 -0400

    feat: crai funoes para navegar entre os pokemons

--- src/view/Pokedex.vue
+++ src/view/Pokedex.vue
@@ -6,13 +6,49 @@
       :src="require('@/assets/pokedex.png')"
     >
     </v-img>
-    <v-card class="pokemon-card"></v-card>
+    <v-card class="pokemon-card">
+      <v-container class="pokemon-container">
+        <img 
+          :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${idPokemon}.gif`"
+          :alt="1"
+          width="50%"
+        />
+        <!-- <h2 class="text-center">1</h2> -->
+      </v-container>
+    </v-card>
+    <v-btn 
+      depressed 
+      class="btn-navigation-previous" 
+      color="error" 
+      @click="previousPokemon()"
+    >
+      previous
+    </v-btn>
+    <v-btn 
+      depressed 
+      class="btn-navigation-next" 
+      color="success" 
+      @click="nextPokemon()"
+    >
+      Next
+    </v-btn>
   </v-container>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    idPokemon: 1
+  }),
 
+  methods:{
+    nextPokemon(){
+      this.idPokemon++
+    },
+    previousPokemon(){
+      this.idPokemon--
+    }
+  }
 }
 </script>
 
@@ -21,6 +57,10 @@ export default {
     margin: 0 auto !important;
     z-index: 2;
   }
+  .pokemon-container{
+    display: flex;
+    justify-content: center;
+  }
   .pokemon-card{
     background-color: #fff;
     width: 255px;
@@ -28,5 +68,21 @@ export default {
     position: absolute;
     top: 365px;
     left: 635px;
+  }
+  .btn-navigation-previous{
+    position: absolute;
+    bottom: 244px;
+        right: 748px;
+    height: 45px !important;
+    min-width: 120px !important;
+    z-index: 3;
+  }
+  .btn-navigation-next{
+    position: absolute;
+    bottom: 244px;
+    right: 610px;
+    height: 45px !important;
+    min-width: 120px !important;
+    z-index: 3;
   }
 </style>
