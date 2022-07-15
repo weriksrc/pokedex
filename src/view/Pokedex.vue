c1410718499b4c53a203dd6c45e758c78ba0a67f
weriks <ti15@viacometa.com.br>
2022-07-15 16:49:39 -0400

    refactor: separa componente de visualizacao de pokemon, e crai funcao para captuar informacoes dos pokemons

--- src/view/Pokedex.vue
+++ src/view/Pokedex.vue
@@ -6,28 +6,20 @@
       :src="require('@/assets/pokedex.png')"
     >
     </v-img>
-    <v-card class="pokemon-card">
-      <v-container class="pokemon-container">
-        <img 
-          :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${idPokemon}.gif`"
-          :alt="1"
-          width="50%"
-        />
-        <!-- <h2 class="text-center">1</h2> -->
-      </v-container>
-    </v-card>
-    <v-btn 
-      depressed 
+    <ScreenPokemon 
+      :idPokemon="idPokemon"
+    />
+    <ScreenNamePokemon 
+      :namePokemon="pokemons.name"
+    />
+    <v-btn
       class="btn-navigation-previous" 
-      color="error" 
       @click="previousPokemon()"
     >
       previous
     </v-btn>
     <v-btn 
-      depressed 
       class="btn-navigation-next" 
-      color="success" 
       @click="nextPokemon()"
     >
       Next
@@ -36,19 +28,43 @@
 </template>
 
 <script>
+import axios from 'axios'
+import ScreenPokemon from "@/components/ScreenPokemon"
+import ScreenNamePokemon from "@/components/ScreenNamePokemon"
 export default {
+  components:{
+    ScreenPokemon,
+    ScreenNamePokemon,
+  },
+
   data: () => ({
-    idPokemon: 1
+    idPokemon: 1,
+    pokemons: []
   }),
 
+  mounted(){
+    this.fetchPokemon(this.idPokemon)
+  },
+
   methods:{
+    async fetchPokemon(id){
+      await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+        this.pokemons = response.data;
+      })
+    },
+
     nextPokemon(){
       this.idPokemon++
+      this.fetchPokemon(this.idPokemon)
     },
+
     previousPokemon(){
-      this.idPokemon--
+      if(this.idPokemon != 1) return this.idPokemon--
+      this.fetchPokemon(this.idPokemon)
     }
-  }
+  },
+
+  
 }
 </script>
 
@@ -57,35 +73,32 @@ export default {
     margin: 0 auto !important;
     z-index: 2;
   }
-  .pokemon-container{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .pokemon-card{
-    display: flex;
-    align-items: center;
-    background-color: #fff;
-    width: 255px;
-    height: 170px;
-    position: absolute;
-    top: 365px;
-    left: 635px;
-  }
+
   .btn-navigation-previous{
     position: absolute;
     bottom: 244px;
     right: 748px;
-    height: 45px !important;
+    height: 43px !important;
     min-width: 120px !important;
+    color: #fff !important;
+    background: #ce0321 !important;
+    border: 2px solid #000606 !important;
+    border-radius: 9px !important;
+    box-shadow: none !important;
     z-index: 3;
   }
+
   .btn-navigation-next{
     position: absolute;
     bottom: 244px;
     right: 610px;
-    height: 45px !important;
+    height: 43px !important;
     min-width: 120px !important;
+    color: #fff !important;
+    background: #0b7082 !important;
+    border: 2px solid #000606 !important;
+    border-radius: 9px !important;
+    box-shadow: none !important;
     z-index: 3;
   }
 </style>
